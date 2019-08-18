cdd2619e1c8dc46a470fb8a6831c760d6c421e88
one0212 <feliciajiang12@gmail.com>
2019-08-19 02:16:17 +0800

    form初步樣式

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ include("config.php");
 session_start();
 // 因預設get方式獲取所以跑到表單的method才會執行以下判斷
 if (isset($_COOKIE['logined'])) {
-    echo "已登入";
+    // echo "已登入";
 } elseif (isset($_POST['account']) && isset($_POST['password'])){
         // 判斷表單內是否有account欄位
     $account = $_POST['account'];
@@ -18,7 +18,7 @@ if (isset($_COOKIE['logined'])) {
     if ($rowNum == 1) {
         setcookie('logined', 'true');
     } else {
-        echo "登入失敗";
+        // echo "登入失敗";
     }
     // if($_POST['account']=='one' and $_POST['password']=='1234');
     // // 找到帳號密碼相符合 
@@ -28,11 +28,6 @@ if (isset($_COOKIE['logined'])) {
 
 }
 ?>
-<?php
-     $result = mysqli_query($db, "SELECT CURRENT_TIMESTAMP");
-     echo mysqli_fetch_assoc($result)["CURRENT_TIMESTAMP"]
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,6 +35,65 @@ if (isset($_COOKIE['logined'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        html {
+           height:100%;
+        }
+        body {
+            min-height:100vh;
+        }
+        header {
+            height: 80px;
+            background:#000;
+        }
+        .container {
+            width:960px;
+            margin:0 auto;
+        }
+        form {
+            background:#ccc;
+            width:300px;
+            height:300px;
+            margin:50px auto;
+            padding:30px;
+            
+        }
+        .b-block {
+            display:block;
+        }
+        label {
+            margin:10px 0;
+            padding:20px 0;
+        }
+        input {
+            /* text-align:center; */
+            width:300px;
+            height:30px;
+            outline:none;
+            background:none;
+            margin:5px 0;
+            border-width:0;
+            border-bottom:1px solid #000;
+        }
+        .submit {
+            /* margin-left:200px;
+            width:100px; */
+            border:1px solid #000;
+            border-radius:5px;
+            text-align:center;
+        }
+        footer {
+            height: 50px;
+            background:#000;
+            position:fixed;
+            bottom:0;
+            left:0;
+            width: 100%;
+           
+        }
+
+  
+    </style>
 </head>
 <body>
 <?php if (isset($_SESSION['user'])): ?>
@@ -47,13 +101,20 @@ if (isset($_COOKIE['logined'])) {
     <p><a href="0816_07_logout.php">登出</a></p>
 
 <?php else: ?>
-<!-- 如果帳密不符合則繼續停留在表單上 -->
-    <form action="" method="post"> 
-    <!-- 傳表單給自己所以不用action -->
-        <input type="text" name="account" placeholder="請輸入帳號"><br>
-        <input type="password" name="password" placeholder="請輸入密碼"><br>
-        <input type="submit">
-    </form>
+<!-- <div class="wrapper"> -->
+    <header></header>
+        <div class="container">
+        <!-- 如果帳密不符合則繼續停留在表單上 -->
+            <form action="" method="post"> 
+            <!-- 傳表單給自己所以不用action -->
+                <label for="" class="b-block">帳號<input class="b-block" type="text" name="account" placeholder=" 請輸入帳號" ></label>
+                <label for="" class="b-block">密碼<input class="b-block" type="password" name="password" placeholder=" 請輸入密碼"></label>
+                <input class="submit" type="text" value="登入">
+            </form>
+        </div>
+    <footer></footer>
+<!-- </div> -->
 <?php endif; ?>
+
 </body>
 </html>
