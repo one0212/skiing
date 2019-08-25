6478ad80bcaa41421d9b440c87903b346b906927
one0212 <feliciajiang12@gmail.com>
2019-08-25 21:55:32 +0800

    require sidebar.php

---
+++ __sidebar.php
@@ -0,0 +1,21 @@
+<div id="wrapper">
+        <aside>
+            <ul id="manage">
+                <li class="item"><a href="#">廠商管理</a></li>
+                <li class="item"><a href="#">會員管理</a></li>
+                <li class="item">
+                    <a href="#">雪場管理</a>   
+                    <div class="submenu">
+                        <a class="submenu-item" href="#">雪場資訊管理</a>
+                        <a class="submenu-item" href="#">教練管理</a>
+                        <a class="submenu-item" href="#">雪具租借管理</a>
+                        <a class="submenu-item" href="#">飯店管理</a>
+                    </div>
+                </li>
+                <li class="item"><a href="#">品牌廠商管理</a></li>
+                <li class="item"><a href="#">景點管理</a></li>
+                <li class="item"><a href="#">訂單管理</a></li> 
+                <li class="item"><a href="#">廠商基本資料</a></li>
+            </ul>
+        </aside>
+    </div>
