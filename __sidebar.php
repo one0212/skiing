5421ae6f5ccd07db0f5dbb52ce65279b41d169a7
one0212 <feliciajiang12@gmail.com>
2019-08-26 22:37:37 +0800

    sidebar更新_廠商欄位

--- __sidebar.php
+++ __sidebar.php
@@ -1,21 +1,61 @@
-<div id="wrapper">
-        <aside>
-            <ul id="manage">
-                <li class="item"><a href="#">廠商管理</a></li>
-                <li class="item"><a href="#">會員管理</a></li>
-                <li class="item">
-                    <a href="#">雪場管理</a>   
+<!-- <div id="wrapper">
+    <aside>
+        <ul id="manage">
+            <li class="item"><a href="#">廠商管理</a></li>
+            <li class="item"><a href="#">會員管理</a></li>
+            <li class="item">
+                <a href="#">雪場管理</a>   
+                <div class="submenu">
+                    <a class="submenu-item" href="#">雪場資訊管理</a>
+                    <a class="submenu-item" href="#">教練管理</a>
+                    <a class="submenu-item" href="#">雪具租借管理</a>
+                    <a class="submenu-item" href="#">飯店管理</a>
+                </div>
+            </li>
+            <li class="item"><a href="#">品牌廠商管理</a></li>
+            <li class="item"><a href="#">景點管理</a></li>
+            <li class="item"><a href="#">訂單管理</a></li> 
+            <li class="item"><a href="#">廠商基本資料</a></li>
+        </ul>
+    </aside>
+</div> -->
+
+    <aside>
+        <ul id="manage">
+            <li class="item"><a href="#">廠商管理</a></li>
+            <li class="item"><a href="#">會員管理</a></li>
+            <li class="item"><a href="#">雪場資訊管理</a></li>
+            <li class="item"><a href="#">教練管理</a></li>
+            <li class="item">
+                <a href="#">飯店資訊管理</a>
                     <div class="submenu">
-                        <a class="submenu-item" href="#">雪場資訊管理</a>
-                        <a class="submenu-item" href="#">教練管理</a>
-                        <a class="submenu-item" href="#">雪具租借管理</a>
-                        <a class="submenu-item" href="#">飯店管理</a>
-                    </div>
-                </li>
-                <li class="item"><a href="#">品牌廠商管理</a></li>
-                <li class="item"><a href="#">景點管理</a></li>
-                <li class="item"><a href="#">訂單管理</a></li> 
-                <li class="item"><a href="#">廠商基本資料</a></li>
-            </ul>
-        </aside>
-    </div>
+                    <a class="submenu-item" href="#">基本資料</a>
+                    <a class="submenu-item" href="#">房型管理</a>
+                </div>
+            </li>
+            <li class="item"><a href="#">雪具租借管理</a></li>
+            <!-- <li class="item"><a href="#">新增商品</a></li> -->
+            <li class="item"><a href="#">訂單管理</a></li> 
+            <li class="item"><a href="#">品牌廠商管理</a></li>
+            <li class="item"><a href="#">景點管理</a></li>
+            <li class="item"><a href="#">訂單管理</a></li>
+            <li class="item"><a href="#">基本資料</a></li>
+        </ul>
+    </aside>
+    
+        <!-- <div class="form">
+            <form action="">
+                <label for="">公司名稱</label><input type="text">
+                <label for="">統一編號／公司行號</label><input type="text">
+                <label for="">負責人</label><input type="text">
+                <label for="">公司地址</label><input type="text">
+                <label for="">公司網站</label><input type="text">
+                <label for="">聯絡人<input class="checkbox" type="checkbox">同負責人</label></input><input type="text">
+                <label for="">聯絡人電話</label><input type="text">
+                <label for="">聯絡人e-mail</label><input type="text">
+                <label for="">備註</label><input type="text">
+            </form>
+        </div> -->
+        <!-- <input  type="button" value=""> -->
+        <!-- <button class="submit">編輯完成</button> -->
+  
---
+++ __vendor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="vendor.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <div id="wrapper" style="max-width:1024px;display:flex;">
+    <?php
+    include("__sidebar.php");
+    ?>
+        <div class="form">
+            <form action="">
+                <label for="">公司名稱</label><input type="text">
+                <label for="">統一編號／公司行號</label><input type="text">
+                <label for="">負責人</label><input type="text">
+                <label for="">公司地址</label><input type="text">
+                <label for="">公司網站</label><input type="text">
+                <label for="">聯絡人<input class="checkbox" type="checkbox">同負責人</label><input type="text">
+                <label for="">聯絡人電話</label><input type="text">
+                <label for="">聯絡人e-mail</label><input type="text">
+                <label for="">備註</label><input type="text">
+            </form>
+        </div>
+        <!-- <input  type="button" value=""> -->
+        <button class="submit">編輯完成</button>
+    </div>
+</body>
+</html>
