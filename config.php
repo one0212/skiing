95a5ff74b153ecd581dac8e2c6033e7d46a7f227
Jason00blabla <cuhaoyan@gmail.com>
2019-08-27 21:41:28 +0800

    fix - 登入成功後，跳轉到 home.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // 建立資料庫連線
-if (empty($db)) {
+try {
 	$userDir = getenv('HOME');
 	$config = parse_ini_file("$userDir" . DIRECTORY_SEPARATOR . "ski_db_conf.ini");
 	$db = New PDO($config['dsn'],$config['username'],$config['password']);
+} catch (PDOException $e) {
+    error_log("PDO 連接資料庫失敗: " . $e->getMessage());
+    die("PDO 連接資料庫失敗: " . $e->getMessage());
 }
 
-// 測試連線  XAMPP的php版本，pdo 沒有 connect_error 這個屬性
-//if ($db->connect_error) {
-//    die("連接失敗: " . $db->connect_error);
-//} 
+error_log("PDO 連接資料庫成功");
 unset($userDir, $config);
 ?>
