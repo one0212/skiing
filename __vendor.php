941835a3d6b76ac4454b8706efb7b14ce2eb101a
one0212 <feliciajiang12@gmail.com>
2019-08-27 09:03:12 +0800

    廠商欄位

--- __vendor.php
+++ __vendor.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="CSS/reset.css">
-    <link rel="stylesheet" href="sidebar.css">
-    <link rel="stylesheet" href="vendor.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/vendor.css">
     
     <title>Document</title>
 </head>
@@ -17,15 +17,16 @@
     ?>
         <div class="form">
             <form action="">
-                <label for="">公司名稱</label><input type="text">
-                <label for="">統一編號／公司行號</label><input type="text">
-                <label for="">負責人</label><input type="text">
-                <label for="">公司地址</label><input type="text">
-                <label for="">公司網站</label><input type="text">
-                <label for="">聯絡人<input class="checkbox" type="checkbox">同負責人</label><input type="text">
-                <label for="">聯絡人電話</label><input type="text">
-                <label for="">聯絡人e-mail</label><input type="text">
-                <label for="">備註</label><input type="text">
+                <label for="">公司名稱</label><input type="text" name="name">
+                <label for="">統一編號／公司行號</label><input type="text" name="company">
+                <label for="">帳戶資訊</label><input type="text" name="bank_account">
+                <label for="">負責人</label><input type="text" name="principal">
+                <label for="">公司地址</label><input type="text" name="">
+                <label for="">公司網站</label><input type="text" name="">
+                <label for="">聯絡人<input class="checkbox" type="checkbox">同負責人</label><input type="text" name="">
+                <label for="">聯絡人電話</label><input type="text" name="">
+                <label for="">聯絡人e-mail</label><input type="text" name="">
+                <label for="">備註</label><input type="text" name="">
             </form>
         </div>
         <!-- <input  type="button" value=""> -->
