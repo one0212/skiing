8106fc247cc2b7b2ab93adf3288675317f79ef62
one0212 <feliciajiang12@gmail.com>
2019-08-28 22:28:26 +0800

    修改classname

--- login.php
+++ login.php
@@ -34,15 +34,13 @@ if (isset($_SESSION['bid'])) {
 </head>
 <body>
     <header></header>
-    <div class="container">
     <!-- 如果帳密不符合則繼續停留在表單上 -->
-        <form action="login.php" method="post"> 
+        <form class="login-form" action="login.php" method="post"> 
         <!-- 傳表單給自己所以不用action -->
-            <label for="" class="b-block">帳號<input class="b-block" type="text" name="account" placeholder=" 請輸入帳號"/>
-            <label for="" class="b-block">密碼<input class="b-block" type="password" name="password" placeholder=" 請輸入密碼"/>
-            <input class="submit" type="submit" value="登入"/>
+            <label for="account" class="b-block login-label">帳號<input class="b-block login-input" type="text" name="account" placeholder=" 請輸入帳號"/>
+            <label for="password" class="b-block login-label">密碼<input class="b-block login-input" type="password" name="password" placeholder=" 請輸入密碼"/>
+            <button class="login-button" type="submit">登入</button>
         </form>
-    </div>
     <footer></footer>
 </body>
 </html>
