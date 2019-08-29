856dad6292ec363adf67965be6ba751d7c2346fd
one0212 <feliciajiang12@gmail.com>
2019-08-30 02:07:46 +0800

    廠商資料匯入資料庫

--- include/__sidebar.php
+++ include/__sidebar.php
@@ -1,21 +1,21 @@
 <!-- <div id="wrapper">
     <aside>
         <ul id="manage">
-            <li class="item"><a href="#">廠商管理</a></li>
-            <li class="item"><a href="#">會員管理</a></li>
-            <li class="item">
+            <li class="sidebar-item"><a href="#">廠商管理</a></li>
+            <li class="sidebar-item"><a href="#">會員管理</a></li>
+            <li class="sidebar-item">
                 <a href="#">雪場管理</a>   
                 <div class="submenu">
-                    <a class="submenu-item" href="#">雪場資訊管理</a>
-                    <a class="submenu-item" href="#">教練管理</a>
-                    <a class="submenu-item" href="#">雪具租借管理</a>
-                    <a class="submenu-item" href="#">飯店管理</a>
+                    <a class="submenu-sidebar-item" href="#">雪場資訊管理</a>
+                    <a class="submenu-sidebar-item" href="#">教練管理</a>
+                    <a class="submenu-sidebar-item" href="#">雪具租借管理</a>
+                    <a class="submenu-sidebar-item" href="#">飯店管理</a>
                 </div>
             </li>
-            <li class="item"><a href="#">品牌廠商管理</a></li>
-            <li class="item"><a href="#">景點管理</a></li>
-            <li class="item"><a href="#">訂單管理</a></li> 
-            <li class="item"><a href="#">廠商基本資料</a></li>
+            <li class="sidebar-item"><a href="#">品牌廠商管理</a></li>
+            <li class="sidebar-item"><a href="#">景點管理</a></li>
+            <li class="sidebar-item"><a href="#">訂單管理</a></li> 
+            <li class="sidebar-item"><a href="#">廠商基本資料</a></li>
         </ul>
     </aside>
 </div> -->
@@ -26,24 +26,25 @@
 
     <aside>
         <ul id="manage">
-            <li class="item"><a href="#">廠商管理</a></li>
-            <li class="item"><a href="#">會員管理</a></li>
-            <li class="item"><a href="ski_areas.php">雪場資訊管理</a></li>
-            <li class="item"><a href="#">教練管理</a></li>
-            <li class="item">
-                <a href="#">飯店資訊管理</a>
-                    <div class="submenu">
+            <li class="sidebar-item"><a href="#">廠商管理</a></li>
+            <li class="sidebar-item"><a href="#">會員管理</a></li>
+            <li class="sidebar-item"><a href="ski_areas.php">雪場資訊管理</a></li>
+            <li class="sidebar-item"><a href="#">教練管理</a></li>
+            <li class="sidebar-item">
+                <a href="#">飯店資訊管理
+                    <div class="sidebar-submenu">
                     <a class="submenu-item" href="#">基本資料</a>
                     <a class="submenu-item" href="#">房型管理</a>
-                </div>
+                    </div>
+                </a>
             </li>
-            <li class="item"><a href="#">雪具租借管理</a></li>
-            <!-- <li class="item"><a href="#">新增商品</a></li> -->
-            <li class="item"><a href="#">訂單管理</a></li> 
-            <li class="item"><a href="#">品牌廠商管理</a></li>
-            <li class="item"><a href="#">景點管理</a></li>
-            <li class="item"><a href="#">訂單管理</a></li>
-            <li class="item"><a href="__vendor.php">基本資料</a></li>
+            <li class="sidebar-item"><a href="#">雪具租借管理</a></li>
+            <!-- <li class="sidebar-item"><a href="#">新增商品</a></li> -->
+            <li class="sidebar-item"><a href="#">訂單管理</a></li> 
+            <li class="sidebar-item"><a href="#">品牌廠商管理</a></li>
+            <li class="sidebar-item"><a href="#">景點管理</a></li>
+            <li class="sidebar-item"><a href="#">訂單管理</a></li>
+            <li class="sidebar-item"><a href="vendor.php">基本資料</a></li>
         </ul>
     </aside>
 
