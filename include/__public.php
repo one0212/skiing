7f18fc2a59011ea9ca8cf504e47578bbe7700100
one0212 <feliciajiang12@gmail.com>
2019-08-29 17:51:52 +0800

    公版製作

---
+++ include/__public.php
@@ -0,0 +1,25 @@
+<?php require("config.php"); 
+// 連線資料
+
+
+// 自己的php
+
+
+?>
+
+<?php include("include/__head.php"); ?>
+<!-- HTML開頭＋link -->
+<?php include("include/__navbar.php"); ?>
+<!-- 導覽列 bootstrap的code -->
+
+<div id="wrapper" style="max-width:1024px;display:flex;">
+<?php include("include/__sidebar.php"); ?>
+<!-- 側邊欄 -->
+
+
+
+<!-- 自己的html,css   code放這邊 -->
+
+
+</div>
+<?php include("include/__footer.php"); ?>
