bbb0c81d8ca62447dd9275361fa7a3c4133592af
Jason00blabla <cuhaoyan@gmail.com>
2019-09-02 01:22:45 +0800

    fix - 動態側邊欄

--- include/__sidebar.php
+++ include/__sidebar.php
@@ -1,68 +1,39 @@
-<!-- <div id="wrapper">
-    <aside>
-        <ul id="manage">
-            <li class="sidebar-item"><a href="#">廠商管理</a></li>
-            <li class="sidebar-item"><a href="#">會員管理</a></li>
-            <li class="sidebar-item">
-                <a href="#">雪場管理</a>   
-                <div class="submenu">
-                    <a class="submenu-sidebar-item" href="#">雪場資訊管理</a>
-                    <a class="submenu-sidebar-item" href="#">教練管理</a>
-                    <a class="submenu-sidebar-item" href="#">雪具租借管理</a>
-                    <a class="submenu-sidebar-item" href="#">飯店管理</a>
-                </div>
-            </li>
-            <li class="sidebar-item"><a href="#">品牌廠商管理</a></li>
-            <li class="sidebar-item"><a href="#">景點管理</a></li>
-            <li class="sidebar-item"><a href="#">訂單管理</a></li> 
-            <li class="sidebar-item"><a href="#">廠商基本資料</a></li>
-        </ul>
-    </aside>
-</div> -->
-   
-   
+<?php 
+require_once("config.php");
+
+$bid = $_SESSION['bid'];
+$sql = "SELECT mf.name, mf.path" .
+    " FROM SKI.MGNT_FUNCTION mf INNER JOIN SKI.MGNT_ADMIN_FUNCTION maf ON maf.FUNC_CODE = mf.CODE" .
+    " WHERE maf.BID = '$bid'";
+$rows = $db->query($sql)->fetchAll();
+ ?>
 
 <link rel="stylesheet" href="css/sidebar.css">
     <aside>
         <ul id="manage">
-            <li class="sidebar-item"><a href="mgnt_vandor.php">廠商管理</a></li>
-            <li class="sidebar-item"><a href="#">會員管理</a></li>
-            <li class="sidebar-item"><a href="ski_areas.php">雪場資訊管理</a></li>
-            <li class="sidebar-item"><a href="#">教練管理</a></li>
-            <li class="sidebar-item">
-                <a href="#">飯店資訊管理
-                    <div class="sidebar-submenu">
-                    <a class="submenu-item" href="#">基本資料</a>
-                    <a class="submenu-item" href="#">房型管理</a>
-                    </div>
-                </a>
-            </li>
-            <li class="sidebar-item"><a href="#">雪具租借管理</a></li>
-            <!-- <li class="sidebar-item"><a href="#">新增商品</a></li> -->
-            <li class="sidebar-item"><a href="#">品牌廠商管理</a></li>
-            <li class="sidebar-item"><a href="#">景點管理</a></li>
-            <li class="sidebar-item"><a href="#">訂單管理</a></li> 
-            <li class="sidebar-item"><a href="vendor.php">基本資料</a></li>
+        <?php
+        foreach($rows as $row) {
+            $function_name = $row['name'];
+            $function_path = $row['path'];
+            echo "<li class='sidebar-item'><a href='$function_path'>$function_name</a></li>";
+            // <li class="sidebar-item"><a href="#">會員管理</a></li>
+            // <li class="sidebar-item"><a href="ski_areas.php">雪場資訊管理</a></li>
+            // <li class="sidebar-item"><a href="#">教練管理</a></li>
+            // <li class="sidebar-item">
+            //     <a href="#">飯店資訊管理
+            //         <div class="sidebar-submenu">
+            //         <a class="submenu-item" href="#">基本資料</a>
+            //         <a class="submenu-item" href="#">房型管理</a>
+            //         </div>
+            //     </a>
+            // </li>
+            // <li class="sidebar-item"><a href="#">雪具租借管理</a></li>
+            // <!-- <li class="sidebar-item"><a href="#">新增商品</a></li> -->
+            // <li class="sidebar-item"><a href="#">品牌廠商管理</a></li>
+            // <li class="sidebar-item"><a href="#">景點管理</a></li>
+            // <li class="sidebar-item"><a href="#">訂單管理</a></li> 
+            // <li class="sidebar-item"><a href="vendor.php">基本資料</a></li>
+        }
+        ?>
         </ul>
     </aside>
-
-
-
-    
-
-        <!-- <div class="form">
-            <form action="">
-                <label for="">公司名稱</label><input type="text">
-                <label for="">統一編號／公司行號</label><input type="text">
-                <label for="">負責人</label><input type="text">
-                <label for="">公司地址</label><input type="text">
-                <label for="">公司網站</label><input type="text">
-                <label for="">聯絡人<input class="checkbox" type="checkbox">同負責人</label></input><input type="text">
-                <label for="">聯絡人電話</label><input type="text">
-                <label for="">聯絡人e-mail</label><input type="text">
-                <label for="">備註</label><input type="text">
-            </form>
-        </div> -->
-        <!-- <input  type="button" value=""> -->
-        <!-- <button class="submit">編輯完成</button> -->
-  
