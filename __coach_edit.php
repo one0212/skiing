81281f485f83876b1b1ea696882ffbbd44dda5d8
Vader <st9866101@gmail.com>
2019-09-02 20:18:13 +0800

    修正版面 連結

--- __coach_edit.php
+++ __coach_edit.php
@@ -9,12 +9,12 @@ if(empty($sid)) {
     exit;
 }
 $sql = "SELECT * FROM `mgnt_coach` WHERE `sid`=$sid";
-$row = $db->query($sql)->fetch();
-if(empty($row)) {
+$coachrow = $db->query($sql)->fetch();
+if(empty($coachrow)) {
     header('Location: __coachlist.php');
     exit;
 }
-
+// print_r($coachrow);
 // 自己的php
 
 
@@ -25,7 +25,7 @@ if(empty($row)) {
 <?php include("include/__navbar.php"); ?>
 <!-- 導覽列 bootstrap的code -->
 
-<div id="wrapper" style="max-width:1024px;display:flex;">
+<div id="" style="display:flex;">
 <?php include("include/__sidebar.php"); ?>
 <!-- 側邊欄 -->
 <style>
@@ -35,7 +35,7 @@ if(empty($row)) {
 </style>
 <!-- 自己的html,css   code放這邊 -->
 <link rel="stylesheet" href="css/coachcss.css?115">
-<div class="container">
+<div class="container-fluid">
 <!-- <div style="margin-top: 2rem;"> -->
     <div class="row">
         <div class="col">
@@ -44,59 +44,89 @@ if(empty($row)) {
     </div>
     <div class="row">
         <div class="col-md-12">
-            
-                
-                    
                     <form name="formcoach" onsubmit="return checkForm()">
-                        <input type="hidden" name="sid" value="<?= $row['sid'] ?>">
+                    <div class="d-flex justify-content-end ">
+                    <a href="__coach_edit.php" class="page-link" style="color:#aaa"><i class="fas fa-edit" style="color:#aaa; margin:0.1rem;"></i>回編輯</a>
+                </div>
+                        <input type="hidden" name="sid" value="<?= $coachrow['sid'] ?>">
                         <div class="form-group">
                             <label for="name">教練姓名</label>
-                            <input type="text" class="form-control" id="name" name="name" value="<?= htmlentities($row['name']) ?>">
+                            <input type="text" class="form-control" id="name" name="name" value="<?= htmlentities($coachrow['name']) ?>">
                             <small id="nameHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
-                            <label for="male">性別</label>
-                            <input type="text" class="form-control" id="male" name="male" value="<?= htmlentities($row['male']) ?>">
-                            <small id="maleHelp" class="form-text"></small>
+                        <label for="">性別</label>
+                    <div class="from-row">
+                        <div class="custom-control custom-radio custom-control-inline col-md-1">
+                            <input type="radio" id="customRadioInline1" name="male" class="custom-control-input" value="男"  <?php if($coachrow['male']==='男') echo("checked");?>>
+                            <label class="custom-control-label" for="customRadioInline1">男</label>
                         </div>
-                        <div class="form-group">
+                        <div class="custom-control custom-radio custom-control-inline col-md-1">
+                            <input type="radio" id="customRadioInline2" name="male" class="custom-control-input" value="女"<?php if($coachrow['male']==='女') echo("checked");?>>
+                            <label class="custom-control-label" for="customRadioInline2">女</label>
+                        </div>
+                        <!-- <div class="form-group">
                             <label for="lan">使用語言</label>
-                            <input type="text" class="form-control" id="lan" name="lan" value="<?= htmlentities($row['lan']) ?>">
+                            <input type="text" class="form-control" id="lan" name="lan" value="<?= htmlentities($coachrow['lan']) ?>">
                             <small id="lanHelp" class="form-text"></small>
+                        </div> -->
+                        <div class="form-group ">
+                    <label for="">語言</label>
+                    <div class="from-row">
+                        <div class="custom-control custom-radio custom-control-inline col-md-2">
+                            <input type="radio" id="lan1" name="lan" class="custom-control-input" value="中文"<?php if ($coachrow['lan']==='中文') echo ("checked"); ?>>
+                            <label class="custom-control-label" for="lan1">中文</label>
                         </div>
-                        <div class="form-group">
-                            <label for="local"></label>當前雪場</label>
-                            <input type="text" class="form-control" id="local" name="local" value="<?= htmlentities($row['local']) ?>">
-                            <small id="localHelp" class="form-text"></small>
+                        <div class="custom-control custom-radio custom-control-inline col-md-2">
+                            <input type="radio" id="lan2" name="lan" class="custom-control-input" value="日文"<?php if ($coachrow['lan']==='日文') echo ("checked"); ?>>
+                            <label class="custom-control-label" for="lan2">日文</label>
                         </div>
+                        <div class="custom-control custom-radio custom-control-inline col-md-2">
+                            <input type="radio" id="lan3" name="lan" class="custom-control-input" value="英文"<?php if ($coachrow['lan']==='英文') echo ("checked"); ?>>
+                            <label class="custom-control-label" for="lan3">英文</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="">目前雪場</label>
+                    <select name="local">
+                        <option value="札幌國際滑雪場">札幌國際滑雪場</option>
+                        <option value="富良野滑雪場">富良野滑雪場</option>
+                        <option value="上越國際滑雪場">上越國際滑雪場</option>
+                        <option value="妙高杉之原滑雪場">妙高杉之原滑雪場</option>
+                        <option value="苗場滑雪場">苗場滑雪場</option>
+                        <option value="藏王溫泉滑雪場">藏王溫泉滑雪場</option>
+                        <option value="八甲田滑雪場">八甲田滑雪場</option>
+                    </select>
+                </div>
                         <div class="form-group">
                             <label for="License">證照</label>
-                            <input type="text" class="form-control" id="License" name="License" value="<?= htmlentities($row['License']) ?>">
+                            <input type="text" class="form-control" id="License" name="License" value="<?= htmlentities($coachrow['License']) ?>">
                             <small id="LicenseHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
                             <label for="skiage">年資</label>
-                            <input type="text" class="form-control" id="skiage" name="skiage" value="<?= htmlentities($row['skiage']) ?>">
+                            <input type="text" class="form-control" id="skiage" name="skiage" value="<?= htmlentities($coachrow['skiage']) ?>">
                             <small id="skiageHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
                             <label for="Experience">經歷</label>
-                            <input type="text" class="form-control" id="Experience" name="Experience" value="<?= htmlentities($row['Experience']) ?>">
+                            <input type="text" class="form-control" id="Experience" name="Experience" value="<?= htmlentities($coachrow['Experience']) ?>">
                             <small id="ExperienceHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
                             <label for="idea">理念</label>
-                            <input type="text" class="form-control" id="idea" name="idea" value="<?= htmlentities($row['idea']) ?>">
+                            <input type="text" class="form-control" id="idea" name="idea" value="<?= htmlentities($coachrow['idea']) ?>">
                             <small id="ideaHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
                             <label for="skiclass">課程</label>
-                            <input type="text" class="form-control" id="skiclass" name="skiclass" value="<?= htmlentities($row['skiclass']) ?>">
+                            <input type="text" class="form-control" id="skiclass" name="skiclass" value="<?= htmlentities($coachrow['skiclass']) ?>">
                             <small id="skiclassHelp" class="form-text"></small>
                         </div>
                         <div class="form-group">
                             <label for="price">價格</label>
-                            <input type="text" class="form-control" id="price" name="price" value="<?= htmlentities($row['price']) ?>">
+                            <input type="text" class="form-control" id="price" name="price" value="<?= htmlentities($coachrow['price']) ?>">
                             <small id="priceHelp" class="form-text"></small>
                         </div>
                         
@@ -127,16 +157,6 @@ if(empty($row)) {
                 info: '請填寫正確的姓名'
             },
    
-            // {
-            //     id: 'class',
-            //     pattern: /^\S{2,}/,
-            //     info: '請填寫課程內容'
-            // },
-            // {
-            //     id: 'price',
-            //     pattern: /^\S{2,}/,
-            //     info: '請填寫每堂價格'
-            // },
         ];
         for(s in required_fields){
             item = required_fields[s];
--- __coachlist.php
+++ __coachlist.php
@@ -7,7 +7,7 @@ $page_title = '資料列表';
 
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
-$per_page = 5; // 每一頁要顯示幾筆
+$per_page = 10; // 每一頁要顯示幾筆
 
 $t_sql = 'SELECT COUNT(1) FROM `mgnt_coach` '; // 拿到總筆數
 
@@ -22,15 +22,15 @@ if ($page < 1) {
     exit;
 }
 if ($page > $totalPages) {
-    header('Location: __coachlist.php?page='.$totalPages);
+    header('Location: __coachlist.php?page=' . $totalPages);
     exit;
 }
 
-// $sql = sprintf('SELECT * FROM `mgnt_coach` ORDER BY `sid` DESC LIMIT %s, %s',
-//         ($page - 1) * $per_page,
-//             $per_page
-// );
-$sql = 'SELECT * FROM `mgnt_coach`';
+$sql = sprintf('SELECT * FROM `mgnt_coach` ORDER BY `sid` DESC LIMIT %s, %s',
+        ($page - 1) * $per_page,
+            $per_page
+);
+// $sql = 'SELECT * FROM `mgnt_coach`';
 
 $stmt = $db->query($sql); //1.取資料放進SMPT
 // 自己的php
@@ -41,15 +41,16 @@ $stmt = $db->query($sql); //1.取資料放進SMPT
 <!-- HTML開頭＋link -->
 <?php include 'include/__navbar.php'; ?>
 <!-- 導覽列 bootstrap的code -->
-<link rel="stylesheet" href="fontawesome/css/all.css?">
-<!-- <?php echo rand(0, 6); ?> -->
 <!-- <div id="wrapper" style="max-width:1024px;display:flex;"> -->
 <div id="" style="display:flex;">
-<?php include 'include/__sidebar.php'; ?>
-<!-- 側邊欄 -->
-<!-- <div class="container"> -->
-<div style="margin-top: 2rem;">
-    <!-- <nav aria-label="Page navigation example">
+    <?php include 'include/__sidebar.php'; ?>
+    <!-- 側邊欄 -->
+    <div class="container-fluid">
+        <div style="margin-top: 2rem;">
+            <div class="d-flex justify-content-end ">
+                <a href="__coachinsert.php" class="page-link" style="color:#aaa"><i class="fas fa-edit" style="color:#aaa; margin:0.1rem;"></i>新增</a>
+            </div>
+            <nav aria-label="Page navigation example">
         <ul class="pagination">
             <li class="page-item">
                 <a class="page-link" href="?page=<?= $page - 1; ?>">
@@ -59,7 +60,7 @@ $stmt = $db->query($sql); //1.取資料放進SMPT
             <?php
             $p_start = $page - 5;
             $p_end = $page + 5;
-            for ($i = $p_start; $i <= $p_end; ++$i):
+            for ($i = $p_start; $i <= $p_end; ++$i) :
                 if ($i < 1 or $i > $totalPages) {
                     continue;
                 }
@@ -74,66 +75,66 @@ $stmt = $db->query($sql); //1.取資料放進SMPT
                 </a>
             </li>
         </ul>
-    </nav> -->
+    </nav>
 
 
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <th scope="col"><i class="fas fa-trash-alt"></i></th>
-            <th scope="col">#</th>
-            <th scope="col">教練姓名</th>
-            <th scope="col">性別</th>
-            <th scope="col">使用語言</th>
-            <th scope="col">雪場</th>
-            <th scope="col">證照</th>
-            <th scope="col">年資</th>
-            <th scope="col">經歷</th>
-            <th scope="col">理念</th>
-            <th scope="col">課程</th>
-            <th scope="col">價錢</th>
-            <th scope="col"><i class="fas fa-edit"></i></th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php while ($r = $stmt->fetch()) {
-                //    2.將資料放進R的陣列裡
-                //    echo "{$r['name']}{$r['male']} <br>"; 
-                    ?>
-            <tr>
-                <td>
-                    <a href="javascript:delete_one(<?= $r['sid']; ?>)"><i class="fas fa-trash-alt"></i></a>
-                </td>
-                <td><?= $r['sid']; ?></td>
-                <td><?= htmlentities($r['name']); ?></td>
-                <td><?= htmlentities($r['male']); ?></td>
-                <td><?= htmlentities($r['lan']); ?></td>
-                <td><?= htmlentities($r['local']); ?></td>
-                <td><?= htmlentities($r['License']); ?></td>
-                <td><?= htmlentities($r['skiage']); ?></td>
-                <td><?= htmlentities($r['Experience']); ?></td>
-                <td><?= htmlentities($r['idea']); ?></td>
-                <td><?= htmlentities($r['skiclass']); ?></td>
-                <td><?= htmlentities($r['price']); ?></td>
-                <td><a href="__coach_edit.php?sid=<?= $r['sid']; ?>"><i class="fas fa-edit"></i></a>
-                </td>
-            </tr>
-        <?php
-                } ?>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col"><i class="fas fa-trash-alt"></i></th>
+                        <th scope="col">#</th>
+                        <th scope="col">教練姓名</th>
+                        <th scope="col">性別</th>
+                        <th scope="col">使用語言</th>
+                        <th scope="col">雪場</th>
+                        <th scope="col">證照</th>
+                        <th scope="col">年資</th>
+                        <th scope="col">經歷</th>
+                        <th scope="col">理念</th>
+                        <th scope="col">課程</th>
+                        <th scope="col">價錢</th>
+                        <th scope="col"><i class="fas fa-edit"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($r = $stmt->fetch()) {
+                        //    2.將資料放進R的陣列裡
+                        //    echo "{$r['name']}{$r['male']} <br>"; 
+                        ?>
+                        <tr>
+                            <td>
+                                <a href="javascript:delete_one(<?= $r['sid']; ?>)"><i class="fas fa-trash-alt"></i></a>
+                            </td>
+                            <td><?= $r['sid']; ?></td>
+                            <td><?= htmlentities($r['name']); ?></td>
+                            <td><?= htmlentities($r['male']); ?></td>
+                            <td><?= htmlentities($r['lan']); ?></td>
+                            <td><?= htmlentities($r['local']); ?></td>
+                            <td><?= htmlentities($r['License']); ?></td>
+                            <td><?= htmlentities($r['skiage']); ?></td>
+                            <td><?= htmlentities($r['Experience']); ?></td>
+                            <td><?= htmlentities($r['idea']); ?></td>
+                            <td><?= htmlentities($r['skiclass']); ?></td>
+                            <td><?= htmlentities($r['price']); ?></td>
+                            <td><a href="__coach_edit.php?sid=<?= $r['sid']; ?>"><i class="fas fa-edit"></i></a>
+                            </td>
+                        </tr>
+                    <?php
+                    } ?>
 
 
-        </tbody>
-    </table>
-</div>
-
-  
-<!-- </div> -->
+                </tbody>
+            </table>
+        </div>
 
 
-<!-- 自己的html,css   code放這邊 -->
-<script>
+    </div>
+
+
+    <!-- 自己的html,css   code放這邊 -->
+    <script>
         function delete_one(sid) {
-            if(confirm(`確定要刪除編號為 ${sid} 的資料嗎?`)){
+            if (confirm(`確定要刪除編號為 ${sid} 的資料嗎?`)) {
                 location.href = '__coach_delete.php?sid=' + sid;
             }
         }
