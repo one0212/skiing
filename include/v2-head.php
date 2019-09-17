74135c88ca7ec52ae03317dfc30bbf870e409e32
one0212 <feliciajiang12@gmail.com>
2019-09-18 01:17:14 +0800

    public template -v2

---
+++ include/v2-head.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>SKIGO ADMIN</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- google font -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC|Noto+Serif+TC|Roboto:400,700&display=swap"
+        rel="stylesheet"> -->
+    <!-- fontawesome-->
+    <link rel="stylesheet" href="../CSS/all.css">
+    <!-- other -->
+    <link rel="stylesheet" href="../CSS/sidebar-v2.css">
+</head>
+<body>
---
+++ include/v2-sidebar.php
@@ -0,0 +1,101 @@
+<nav class="cc navbar navbar-light">
+    <div class=" info float-right  text-right">
+        <p>BLUEPRINT</p>
+        <h2>Sidebar</h2>
+        <div class="nav-btn">
+            <a href=""><i class="fas fa-user-circle fa-lg"></i></a>
+            <a href=""><i class="fas fa-user-circle fa-lg"></i></a>
+            <a href=""><i class="fas fa-user-circle fa-lg"></i></a>
+        </div>
+    </div>
+</nav>
+
+<aside id="sidebar">
+        <div class="sidebar-header">
+            <a href=""><img class="svg-logo" src="../images/skigo-logo.svg" alt="logo"></a>
+        </div>
+        <ul>
+            <li>
+                <a href="#User-Submenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-user-friends"></i>會員控管<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse " id="User-Submenu">
+                    <li>
+                        <a href="#">權限控管</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-mountain"></i>景點管理</a>
+            </li>
+            <li>
+                <a href="#Submenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-skiing"></i>雪場管理<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse" id="Submenu">
+                    <li>
+                        <a href="#" >雪場資料</a>
+                    </li>
+                    <li>
+                        <a href="#">新增票券</a>
+                    </li>
+
+                </ul>
+            </li>
+            <li>
+                <a href="#hotelSubmenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-igloo"></i>住宿管理<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse" id="hotelSubmenu">
+                    <li>
+                        <a href="#">飯店資料</a>
+                    </li>
+                    <li>
+                        <a href="#">客房管理</a>
+                    </li>
+                    <li class="Submenu-border">
+                        <a href="#">新增房型</a>
+                    </li>
+
+                </ul>
+            </li>
+            <li>
+                <a href="#BrandSubmenu" data-toggle="collapse" aria-expanded="false"><i
+                        class="fas fa-hands-helping"></i>品牌廠商<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse" id="BrandSubmenu">
+                    <li>
+                        <a href="#">品牌管理</a>
+                    </li>
+                    <li class="Submenu-border">
+                        <a href="#">新增商品</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#ticket-Submenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-ticket-alt"></i>雪場課程<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse" id="ticket-Submenu">
+                    <li>
+                        <a href="#">教練管理</a>
+                    </li>
+                    <li>
+                        <a href="#">課程管理</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#Brand2Submenu" data-toggle="collapse" aria-expanded="false">
+                    <i class="fas fa-bullhorn"></i>行銷活動<i class="fas fa-ellipsis-h Submenu-end"></i></a>
+                <ul class="collapse" id="Brand2Submenu">
+                    <li>
+                        <a href="#">我的廣告</a>
+                    </li>
+                    <li>
+                        <a href="#">折扣活動</a>
+                    </li>
+                    <li>
+                        <a href="#">主題活動</a>
+                    </li>
+                    <!-- <li class="Submenu-border">
+                        <a href="#">折扣券</a>
+                    </li> -->
+                </ul>
+            </li>
+    </aside>
